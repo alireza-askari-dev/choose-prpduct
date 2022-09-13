b7da393609ba70fb1619de1b2abfad9266f619c5
Alireza-Asakri <alireza.askari.dev@gmail.com>
2022-09-13 23:19:55 +0430

    new

--- components/common/MainMenu.jsx
+++ components/common/MainMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Link from "next/link";
 
 // mrx : material ui ↓
@@ -8,43 +8,53 @@ import { Grid, IconButton } from "@material-ui/core";
 import Style from "../../styles/main/main.module.css";
 
 // mrx : files ↓
-import HomeSvg from "../../public/assets/Icons/Home.svg";
+import HomeSvg from "../../public/assets/Icons/Home-light.svg";
+import HomeSelectedSVG from "../../public/assets/Icons/Home-selected.svg";
+
 import BellSvg from "../../public/assets/Icons/Bell.svg";
 import ContactsSvg from "../../public/assets/Icons/Contacts.svg";
 import OrdersSvg from "../../public/assets/Icons/Orders.svg";
 import GallerySvg from "../../public/assets/Icons/Gallery.svg";
 
+// mrx : context ↓
+import { Context } from "../../context/index";
+import { Router } from "next/router";
+
 // mrx : components ↓
 
 export default function MainMenu() {
   // gm : states ↓
+  const {
+    Menu,
+    setMenu,
+  } = useContext(Context);
 
   return (
     <div className={Style.MainMenu + " cssanimation sequence fadeInBottom"}>
       <div className={Style.C_MainMenu}>
         <div className={Style.P_IconMenu}>
           <IconButton size="small">
-            <img src={BellSvg.src} />
+            <img src={Menu == 4 ? HomeSelectedSVG.src : HomeSvg.src} />
           </IconButton>
         </div>
         <div className={Style.P_IconMenu}>
           <IconButton size="small">
-            <img src={ContactsSvg.src} />
+            <img src={Menu == 3 ? HomeSelectedSVG.src : HomeSvg.src} />
           </IconButton>
         </div>
         <div className={Style.P_IconMenu}>
           <IconButton size="small">
-            <img src={OrdersSvg.src} />
+            <img src={Menu == 2 ? HomeSelectedSVG.src : HomeSvg.src} />
           </IconButton>
         </div>
         <div className={Style.P_IconMenu}>
-          <IconButton size="small">
-            <img src={GallerySvg.src} />
+          <IconButton onClick={() => { setMenu(1) }} size="small">
+            <img src={Menu == 1 ? HomeSelectedSVG.src : HomeSvg.src} />
           </IconButton>
         </div>
         <div className={Style.P_IconMenu}>
-          <IconButton size="small">
-            <img src={HomeSvg.src} />
+          <IconButton onClick={() => { setMenu(0) }} size="small">
+            <img src={Menu == 0 ? HomeSelectedSVG.src : HomeSvg.src} />
           </IconButton>
         </div>
       </div>
--- context/index.js
+++ context/index.js
@@ -3,16 +3,21 @@ import react, { createContext, useState } from 'react';
 export const Context = react.createContext({
     Example: [],
     setExample: [],
+    Menu: 0,
+    setMenu: 0,
 });
 
 const ContextProvider = (props) => {
-    const [Example, setExample] = react.useState([]);
+    const [Example, setExample] = useState([]);
+    const [Menu, setMenu] = useState(0)
 
     return (
         <Context.Provider
             value={{
                 Example,
                 setExample,
+                setMenu,
+                Menu
             }}
         >
             {props.children}
